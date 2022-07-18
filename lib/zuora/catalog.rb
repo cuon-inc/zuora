c29e0a126a7adb3152b7b2ee5c068d1a9b0cc9bb
rakio1234 <yuta.uchida0101@gmail.com>
2022-07-18 22:59:48 +0900

    catlogにfind_byメソッド追加

--- lib/zuora/catalog.rb
+++ lib/zuora/catalog.rb
@@ -7,6 +7,11 @@ module Zuora
     end
 
     # @return [Hash]
+    def self.find_by(**args)
+      Zuora::Core.find_by("Catalog", **args)
+    end
+
+    # @return [Hash]
     def self.last
       records = Zuora::Api::V1::Action.query("select id, sku, name, CreatedDate from Product")["records"]
       id = records.max_by { |h| h["CreatedDate"] }["Id"]
